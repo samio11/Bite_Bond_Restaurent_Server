01fd1653f2a0e53f041d1c9caeaf0533d688ab8f
samio11 <samiohasan6@gmail.com>
2024-08-24 13:50:29 +0600

    Menu Section Done

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const corsConfig = {
-    origin: ['http://localhost:5173','http://localhost:5174'],
-    credentials: true,
-    optionsSuccessStatus: 200,
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionsSuccessStatus: 200,
 }
 app.use(cors(corsConfig));
 app.use(express.json())
 
-app.get('/',async(req,res)=>{
-    res.send('Hello Server is Running')
+app.get('/', async (req, res) => {
+  res.send('Hello Server is Running')
 })
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mmutbdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -25,18 +25,55 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-const food_Collection  = client.db("Bite_Bond").collection("Foods_Collection")
+const food_Collection = client.db("Bite_Bond").collection("Foods_Collection")
+const food_Rating_Collection = client.db("Bite_Bond").collection("Food_Rating")
 async function run() {
   try {
     // Home page Our Menu Section
-    app.get('/home-menu',async(req,res)=>{
-        const  query = {
-            food_price: {$gt:5.00,$lt:10.00}
-        }
-        const result = await food_Collection.find(query).limit(6).toArray()
-        res.send(result)
-
+    app.get('/home-menu', async (req, res) => {
+      const query = {
+        food_price: { $gt: 5.00, $lt: 10.00 }
+      }
+      const result = await food_Collection.find(query).limit(6).toArray()
+      res.send(result)
     })
+    // Home Page Chef Recommend
+    app.get('/home-chef-recommend', async (req, res) => {
+      const query = {
+        food_rating: { $gt: 4.6 }
+      }
+      const result = await food_Collection.find(query).limit(3).toArray()
+      res.send(result)
+    })
+
+    // For Home Page
+    app.get('/food-rating', async (req, res) => {
+      const result = await food_Rating_Collection.find().limit(3).toArray()
+      res.send(result)
+    })
+
+    // For Menu Page
+    app.get('/menu-top-foods', async (req, res) => {
+      const query = {
+        food_rating: { $gt: 4.7 }
+      }
+      const result = await food_Collection.find(query).limit(4).toArray()
+      res.send(result)
+    })
+
+    // For Menu category wise data Load by query
+    app.get('/selected-category-food',async(req,res)=>{
+      const { category } = req.query
+      const query = {
+        food_category: category
+      }
+      const result = await food_Collection.find(query).toArray()
+      res.send(result)
+    })
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -48,5 +85,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
