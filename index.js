dc4a546fe07222ff67a5c68885fc1d2cb26da46f
samio11 <samiohasan6@gmail.com>
2024-08-26 14:19:21 +0600

    Code for simplify admin and guest done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Timestamp } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const corsConfig = {
@@ -28,6 +28,7 @@ const client = new MongoClient(uri, {
 const food_Collection = client.db("Bite_Bond").collection("Foods_Collection")
 const food_Rating_Collection = client.db("Bite_Bond").collection("Food_Rating")
 const food_Ordering_Collection = client.db("Bite_Bond").collection("Food_Ordered")
+const usersCollection = client.db("Bite_Bond").collection("Users");
 async function run() {
   try {
     // Home page Our Menu Section
@@ -76,6 +77,22 @@ async function run() {
       res.send(result);
     })
 
+    // login->> userCollection store user Data and also role
+    app.put('/user', async (req, res) => {
+      const user = req.body;
+      const isExist = await usersCollection.findOne({ email: user?.email })
+      if (isExist) return res.send(isExist)
+      const options = { upsert: true }
+      const query = { email: user.email }
+      const updateUser = {
+        $set: {
+          ...user, Timestamp: Date.now()
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateUser, options)
+      res.send(result);
+    })
+
     // Get all review data to disable button
     app.get('/ratingData', async (req, res) => {
       const result = await food_Rating_Collection.find().toArray()
@@ -90,6 +107,13 @@ async function run() {
       }
       const result = await food_Collection.find(query).toArray()
       res.send(result)
+    })
+
+    // Load Role
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email });
+      res.send(user)
     })
 
 
